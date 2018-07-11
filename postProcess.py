d03719d0bbd1e496023acadc6935819fa6ef4b85
Ruben Van Coile <Ruben.VanCoile@UGent.be>
2018-07-11 15:24:28 -0400

    initialization for development

---
+++ postProcess.py
@@ -0,0 +1,19 @@
+# __author__ = "RVC"
+# __email__= "ruben.vancoile@gmail.com"
+# __date__= "2018-07-11"
+
+#
+# post process SAFIR *.OUT file
+# cfr. limitations capabilities DIAMOND
+#
+
+
+####################
+## MODULE IMPORTS ##
+####################
+
+
+##############
+## FUNCTION ##
+##############
+
